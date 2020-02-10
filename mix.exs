98c0f93e1d85fa41c17f2caff11ce9a06b65a3d9
Martin Grotz <martin.grotz@gmx.de>
2020-02-10 20:38:54 +0100

    push version number for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JpeKartenwunsch.MixProject do
   def project do
     [
       app: :jpe_kartenwunsch,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
